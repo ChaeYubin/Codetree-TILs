4fdbd1bd5ec96f7114662ca3ec5b172168cfb2e3
yubin <cyoobin@naver.com>
2026-01-29 17:24:42 +0900

    Title: 그 요일은, Time: 127ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260129/그 요일은/the-day-of-the-day.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m1 = sc.nextInt();
+        int d1 = sc.nextInt();
+        int m2 = sc.nextInt();
+        int d2 = sc.nextInt();
+        String A = sc.next();
+        // Please write your code here.
+
+        int[] datePerMonth = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        String[] day = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+
+        int day1 = d1;
+
+        for (int i = 0; i < m1 - 1; i++) {
+            day1 += datePerMonth[i];
+        }
+
+        int day2 = d2;
+
+        for (int i = 0; i < m2 - 1; i++) {
+            day2 += datePerMonth[i];
+        }
+
+        int diff = day2 - day1;
+
+        int dayIndex = 0;
+
+        for (int i = 0; i < day.length; i++) {
+            if (A.equals(day[i])) {
+                dayIndex = i;
+                break;
+            }
+        }
+
+        int num = diff / 7;
+        int plus = (diff % 7) >= (dayIndex + 1) ? 0 : 1;
+
+        System.out.println(num + plus);
+    }
+}
