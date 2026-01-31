89bcb36af979f72b0ffa38b1fc6fb1b429abac8a
yubin <cyoobin@naver.com>
2026-01-31 21:23:43 +0900

    Title: 반복 출력하기 2, Time: 125ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260131/반복 출력하기 2/repeated-output-2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+    public static void recursion(int n) {
+        if (n == 0) return;
+
+        recursion(n - 1);
+        System.out.println("HelloWorld");
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        recursion(n);
+    }
+}
