01761166a4caecc518bfa6e0207f1eaa4f548732
yubin <cyoobin@naver.com>
2026-02-08 15:50:59 +0900

    Title: 총점 비교, Time: 132ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260208/총점 비교/compare-total-points.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+    public static class Student implements Comparable<Student> {
+        String name;
+        int korean;
+        int english;
+        int math;
+
+        public Student(String name, int korean, int english, int math) {
+            this.name = name;
+            this.korean = korean;
+            this.english = english;
+            this.math = math;
+        }
+
+        @Override
+        public int compareTo(Student student) {
+            return (this.korean + this.english + this.math) - (student.korean + student.english + student.math);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Student[] students = new Student[n];
+
+        for (int i = 0; i < n; i++) {
+            students[i] = new Student(sc.next(), sc.nextInt(), sc.nextInt(), sc.nextInt());
+        }
+
+        Arrays.sort(students);
+
+        for (int i = 0; i < n; i++) {
+            System.out.println(students[i].name + " " + students[i].korean + " " + students[i].english + " " + students[i].math);
+        }
+    }
+}
