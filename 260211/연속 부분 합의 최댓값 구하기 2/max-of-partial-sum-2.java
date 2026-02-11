e9499d7234dad12d619b49c5300f3ac624521be5
yubin <cyoobin@naver.com>
2026-02-11 16:44:13 +0900

    Title: 연속 부분 합의 최댓값 구하기 2, Time: 383ms, Memory: 23MB, Status: Passed - Codetree

--- 260211/연속 부분 합의 최댓값 구하기 2/max-of-partial-sum-2.java
+++ 260211/연속 부분 합의 최댓값 구하기 2/max-of-partial-sum-2.java
@@ -10,14 +10,14 @@ public class Main {
             a[i] = sc.nextInt();
         }
 
-        int maxSum = 0;
+        int maxSum = Integer.MIN_VALUE;
         int sum = 0;
 
         for (int i = 0; i < N; i++) {
             if (sum < 0) {
                 sum = 0;
             } 
-            
+
             sum += a[i];
 
             maxSum = Math.max(maxSum, sum);
