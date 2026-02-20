b9b2cb51d33de2199149e1fe4bf5e2bbeaf75b75
yubin <cyoobin@naver.com>
2026-02-20 14:06:45 +0900

    Title: 기울어진 직사각형, Time: 87ms, Memory: 9MB, Status: Wrong Answer - Codetree

---
+++ 260220/기울어진 직사각형/slanted-rectangle.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.StreamTokenizer;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StreamTokenizer st = new StreamTokenizer(br);
+
+    final static int[][] DIR = {{-1, 1}, {-1, -1}, {1, -1}, {1, 1}};
+
+    static int N;
+    static int[][] grid;
+    static int max;
+
+    public static void main(String[] args) throws Exception {
+        N = nextInt();
+        grid = new int[N][N];
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                grid[i][j] = nextInt();
+            }
+        }
+
+        max = 0;
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                dfs(i, j, i, j, 0, grid[i][j]);
+            }
+        }
+
+        System.out.println(max);
+    }
+
+    static void dfs(int startX, int startY, int curX, int curY, int prevD, int sum) {
+        for (int i = prevD; i < 4; i++) {
+            int nx = curX + DIR[prevD][0];
+            int ny = curY + DIR[prevD][1];
+
+            if (nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
+
+            // 원점으로 돌아온 경우
+            if (nx == startX && ny == startY) {
+                max = Math.max(max, sum);
+                return;
+            }
+
+            dfs(startX, startY, nx, ny, i, sum + grid[nx][ny]);
+        }
+    }
+
+    static int nextInt() throws IOException {
+        st.nextToken();
+        return (int) st.nval;
+    }
+}
