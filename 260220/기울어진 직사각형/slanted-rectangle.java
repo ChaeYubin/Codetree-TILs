b9446acc64b0e2a3f466a7565a44e08b24bbf4ae
yubin <cyoobin@naver.com>
2026-02-20 14:09:18 +0900

    Title: 기울어진 직사각형, Time: 93ms, Memory: 9MB, Status: Wrong Answer - Codetree

--- 260220/기울어진 직사각형/slanted-rectangle.java
+++ 260220/기울어진 직사각형/slanted-rectangle.java
@@ -27,14 +27,14 @@ public class Main {
 
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-                dfs(i, j, i, j, 0, grid[i][j]);
+                dfs(i, j, i, j, 0, grid[i][j], 0);
             }
         }
 
         System.out.println(max);
     }
 
-    static void dfs(int startX, int startY, int curX, int curY, int prevD, int sum) {
+    static void dfs(int startX, int startY, int curX, int curY, int prevD, int sum, int count) {
         for (int i = prevD; i < 4; i++) {
             int nx = curX + DIR[prevD][0];
             int ny = curY + DIR[prevD][1];
@@ -42,12 +42,12 @@ public class Main {
             if (nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
 
             // 원점으로 돌아온 경우
-            if (nx == startX && ny == startY) {
+            if (nx == startX && ny == startY && count > 2) {
                 max = Math.max(max, sum);
                 return;
             }
 
-            dfs(startX, startY, nx, ny, i, sum + grid[nx][ny]);
+            dfs(startX, startY, nx, ny, i, sum + grid[nx][ny], count + 1);
         }
     }
 
