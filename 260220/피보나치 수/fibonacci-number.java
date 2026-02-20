bf9df2b882ce464b521ca732afe45e692ac14736
yubin <cyoobin@naver.com>
2026-02-20 16:58:10 +0900

    Title: 피보나치 수, Time: 131ms, Memory: 10MB, Status: Passed - Codetree

--- 260220/피보나치 수/fibonacci-number.java
+++ 260220/피보나치 수/fibonacci-number.java
@@ -9,7 +9,10 @@ public class Main {
 
         fibo[0] = 0;
         fibo[1] = 1;
-        fibo[2] = 1;
+
+        if (n >= 2) {
+            fibo[2] = 1;
+        }
 
         for (int i = 3; i <= n; i++) {
             fibo[i] = fibo[i - 1] + fibo[i - 2];
