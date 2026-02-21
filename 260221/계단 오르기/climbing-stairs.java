548577fcc2f43239c35e94b2b6f66beeafb0bd8c
yubin <cyoobin@naver.com>
2026-02-21 14:35:42 +0900

    Title: 계단 오르기, Time: 121ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260221/계단 오르기/climbing-stairs.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        // dp[i]: i층 높이에 올라오는 서로 다른 방법의 수
+        // => (i - 2)층에서 2계단을 올라오는 경우 + (i - 3)층에서 3계단을 올라오는 경우
+        int[] dp = new int[n + 1];
+
+        dp[0] = 1;
+        dp[1] = 0;
+        dp[2] = 1;
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 2] + dp[i - 3];
+        }
+
+        System.out.println(dp[n]);
+    }
+}
