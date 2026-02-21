3086fd4c94495daf00952ba95b6666d6cbfa16fd
yubin <cyoobin@naver.com>
2026-02-21 14:45:02 +0900

    Title: 계단 오르기, Time: 124ms, Memory: 10MB, Status: Passed - Codetree

--- 260221/계단 오르기/climbing-stairs.java
+++ 260221/계단 오르기/climbing-stairs.java
@@ -9,12 +9,12 @@ public class Main {
         // => (i - 2)층에서 2계단을 올라오는 경우 + (i - 3)층에서 3계단을 올라오는 경우
         int[] dp = new int[1001];
 
-        dp[1] = 0;
+        // 초기값 설정
         dp[2] = 1;
         dp[3] = 1;
 
         for (int i = 4; i <= n; i++) {
-            dp[i] = dp[i - 2] + dp[i - 3];
+            dp[i] = (dp[i - 2] + dp[i - 3]) % 10007;
         }
 
         System.out.println(dp[n]);
