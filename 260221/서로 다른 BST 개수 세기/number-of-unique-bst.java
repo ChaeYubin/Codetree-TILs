a6c7789af512ce40b512e4e6f885b3bdbdb4b1c9
yubin <cyoobin@naver.com>
2026-02-21 15:57:23 +0900

    Title: 서로 다른 BST 개수 세기, Time: 123ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260221/서로 다른 BST 개수 세기/number-of-unique-bst.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] dp = new int[20];
+
+        dp[0] = 1;
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            for (int j = 0; j <= n - 1; j++) {
+                dp[i] += dp[j] * dp[n - j - 1];
+            }
+        }
+
+        System.out.println(dp[n]);
+    }
+}
