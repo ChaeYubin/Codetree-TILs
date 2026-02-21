c563529fe8fef0cb4f1802b67aa29d1888405b36
yubin <cyoobin@naver.com>
2026-02-21 15:00:39 +0900

    Title: 사각형 채우기, Time: 129ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 260221/사각형 채우기/rectangle-fill.java
+++ 260221/사각형 채우기/rectangle-fill.java
@@ -11,7 +11,7 @@ public class Main {
         dp[2] = 2;
 
         for (int i = 3; i <= n; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 1007;
         }
 
         System.out.println(dp[n]);
