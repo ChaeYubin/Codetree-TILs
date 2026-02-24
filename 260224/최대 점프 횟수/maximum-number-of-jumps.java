df07ff6a855a4bed85a849af58421768322d6953
yubin <cyoobin@naver.com>
2026-02-24 11:57:03 +0900

    Title: 최대 점프 횟수, Time: 161ms, Memory: 11MB, Status: Passed - Codetree

--- 260224/최대 점프 횟수/maximum-number-of-jumps.java
+++ 260224/최대 점프 횟수/maximum-number-of-jumps.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,16 +10,19 @@ public class Main {
             arr[i] = sc.nextInt();
         }
 
+        // dp[i]: 첫 번째 위치에서 i번째 위치로 오기까지의 최대 점프 횟수
         int[] dp = new int[n + 1];
-        dp[1] = 1;
+        Arrays.fill(dp, -1);
+
+        dp[1] = 0;
         int result = 0;
 
         for (int i = 1; i < n; i++) {
-            if (dp[i] == 0) continue;
+            if (dp[i] == -1) continue;
 
             for (int j = 1; j <= arr[i] && i + j <= n; j++) {
                 dp[i + j] = dp[i] + 1;
-                result = dp[i];
+                result = dp[i + j];
             }
         }
 
