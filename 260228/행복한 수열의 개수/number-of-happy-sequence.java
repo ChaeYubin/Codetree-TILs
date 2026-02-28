338ced2715b4a81d412fed44a80d3f15ffede74b
yubin <cyoobin@naver.com>
2026-02-28 17:06:16 +0900

    Title: 행복한 수열의 개수, Time: 225ms, Memory: 15MB, Status: Passed - Codetree

--- 260228/행복한 수열의 개수/number-of-happy-sequence.java
+++ 260228/행복한 수열의 개수/number-of-happy-sequence.java
@@ -12,11 +12,18 @@ public class Main {
                 grid[i][j] = sc.nextInt();
 
         int happyNumbers = 0;
-        
+
+        // 각 행의 수열 확인
         for (int i = 0; i < n; i++) {
             int count = 1;
+            
+            if (count >= m) {
+                happyNumbers++;
+                continue;
+            }
+
             for (int j = 1; j < n; j++) {
-                if (grid[i][j - 1] == grid[i][j]) {
+                if (grid[j - 1][i] == grid[j][i]) {
                     count++;
                 } else {
                     count = 1;
@@ -24,15 +31,22 @@ public class Main {
 
                 if (count >= m) {
                     happyNumbers++;
-                    j = n;  // j for문 skip
+                    j = n;  
                 }
             }
         }
-
+        
+        // 각 열의 수열 확인
         for (int i = 0; i < n; i++) {
             int count = 1;
+
+            if (count >= m) {
+                happyNumbers++;
+                continue;
+            }
+
             for (int j = 1; j < n; j++) {
-                if (grid[j - 1][i] == grid[j][i]) {
+                if (grid[i][j - 1] == grid[i][j]) {
                     count++;
                 } else {
                     count = 1;
