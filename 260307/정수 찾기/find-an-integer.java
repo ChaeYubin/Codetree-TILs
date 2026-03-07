c42e3250b9066b2c15a5f82c46c60a564f9a0dea
yubin <cyoobin@naver.com>
2026-03-07 11:48:51 +0900

    Title: 정수 찾기, Time: 1641ms, Memory: 53MB, Status: Passed - Codetree

---
+++ 260307/정수 찾기/find-an-integer.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.util.HashSet;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        HashSet<Integer> set = new HashSet<>();
+
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            set.add(sc.nextInt());
+        }
+
+        int m = sc.nextInt();
+        for (int i = 0; i < m; i++) {
+            System.out.println(set.contains(sc.nextInt()) ? 1 : 0);
+        }
+    }
+}
