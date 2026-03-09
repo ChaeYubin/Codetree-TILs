74bbbab59485f2eaa72d4716982a13c481e6e306
yubin <cyoobin@naver.com>
2026-03-09 21:56:35 +0900

    Title: 작지만 큰 숫자, Time: 1493ms, Memory: 45MB, Status: Passed - Codetree

---
+++ 260309/작지만 큰 숫자/small-but-big-number.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+import java.util.TreeSet;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        TreeSet<Integer> set = new TreeSet<>();
+
+        for (int i = 0; i < n; i++) {
+            set.add(sc.nextInt());
+        }
+        
+        for (int i = 0; i < m; i++) {
+            int num = sc.nextInt();
+
+            if (set.floor(num) != null) {
+                System.out.println(set.floor(num));
+                set.remove(set.floor(num));
+            } else {
+                System.out.println(-1);
+            }
+        }
+    }
+}
