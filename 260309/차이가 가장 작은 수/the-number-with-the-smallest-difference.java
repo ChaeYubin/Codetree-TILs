c84299b6536441b25ef7288f6d0706eaca423b85
yubin <cyoobin@naver.com>
2026-03-09 21:27:32 +0900

    Title: 차이가 가장 작은 수, Time: 530ms, Memory: 24MB, Status: Wrong Answer - Codetree

--- 260309/차이가 가장 작은 수/the-number-with-the-smallest-difference.java
+++ 260309/차이가 가장 작은 수/the-number-with-the-smallest-difference.java
@@ -9,26 +9,32 @@ public class Main {
         int n = sc.nextInt();
         int m = sc.nextInt();
 
+        TreeSet<Integer> set = new TreeSet<>();
+        
         int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
+            set.add(arr[i]);
         }
 
-        TreeSet<Integer> diff = new TreeSet<>();
+        int answer = Integer.MAX_VALUE;
 
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i == j) continue;
+            int x = arr[i];
 
-                diff.add(Math.abs(arr[i] - arr[j]));
+            // x보다 m 이상 더 크면서 가장 작은 값
+            // r - x >= m을 만족하는 최소 r ==> r >= m + x를 만족하는 최소 r 구하기
+            if (set.ceiling(m + x) != null) {
+                answer = Math.min(answer, set.ceiling(m + x) - x);
+            }
+
+            // x보다 m 이상 더 작으면서 가장 큰 값
+            // x - r >= m을 만족하는 최대 r ==> x - m >= r을 만족하는 최대 r 구하기
+            if (set.floor(x - m) != null) {
+                answer = Math.min(answer, set.floor(x - m) + m);
             }
         }
 
-        // 차이가 M 이상이면서 제일 작은 경우 -> ceiling
-        if (diff.ceiling(m) != null) {
-            System.out.println(diff.ceiling(m));
-        } else {
-            System.out.println("-1");
-        }
+        System.out.println(answer == Integer.MAX_VALUE ? -1 : answer);
     }
 }
